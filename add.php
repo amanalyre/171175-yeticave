61702a3a7b43f6a973f8e89544a37e6260a18b12
Горева Мария <mfgoreva@avito.ru>
2018-07-19 20:24:06 +0300

    module6-task2: page for adding a new lot

---
+++ add.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once("functions.php");
+require_once ('connection.php');
+
+//if (isAuthorized() === false) {
+//    http_response_code(403); #TODO Было бы хорошо тут редиректить на 403 страницу. Или форму авторизации
+//    exit;
+//}
+
+$categories = getCatList();
+
+if ($_POST) {
+    $lot = $_POST['lot'];
+    $image = $_FILES['photo'];
+
+    $resultAddLot = saveLot($lot, $image); //#TODO это поле должно совпадать с name в форме шаблона
+
+    if (is_numeric($resultAddLot)) {
+        header('Location: lot.php?id=' . $resultAddLot);
+    } else {
+        $errors = $resultAddLot;
+    }
+}
+try {
+    $templContent = renderTemplate('add-lot', [
+        'name'        => $lot['name'] ?? '',
+        'category'    => $lot['category'] ?? '',
+        'categories'  => $categories,
+        'description' => $lot['description'] ?? '',
+        'start_price'        => $lot['start_price'] ?? '',
+        'step'        => $lot['step'] ?? '',
+        'finish_date'        => $lot['finish_date'] ?? '',
+        'errors'      => $errors ?? [],
+        'photo'       => $_FILES['photo']
+    ]);
+} catch (Exception $e)
+{
+    echo 'Поймано исключение: ',  $e->getMessage(), "\n";
+};
+
+
+$layoutContent = renderTemplate('layout', [
+    'pageContent' => $templContent,
+    'categories'  => $categories,
+    'pageName'    => 'Добавление нового лота']);
+    //'isAuth' => empty($_SESSION['user']) ? false : true,
+    //'userName' => $_SESSION['user']['name'] ?? null,
+    //'userAvatar' => $_SESSION['user']['avatar'] ?? null]);
+
+print($layoutContent);
--- functions.php
+++ functions.php
@@ -41,10 +41,13 @@ function processingSqlQuery(array $parameterList, $db = null)
     $stmt = db_get_prepare_stmt($db, $parameterList['sql'], $parameterList['data']);
     mysqli_stmt_execute($stmt);
     $res = mysqli_stmt_get_result($stmt);
-    if (mysqli_num_rows($res) > 1) {
-        $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
-    } else {
-        $result = mysqli_fetch_array($res, MYSQLI_ASSOC);
+    $result = true;
+    if ($res != false) {
+        if (mysqli_num_rows($res) > 1) {
+            $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        } else {
+            $result = mysqli_fetch_array($res, MYSQLI_ASSOC);
+        }
     }
     return $result;
 }
@@ -60,7 +63,7 @@ function addLimit(array $parameterList)
 }
 // Здесь получается список категорий
 function getCatList(int $limit = null, $db = null) {
-    $sql = 'SELECT `cat_name` FROM categories;';
+    $sql = 'SELECT `cat_name`, `id` FROM categories;';
     $parameterList = [
         'sql' => $sql,
         'data' => [],
@@ -69,7 +72,13 @@ function getCatList(int $limit = null, $db = null) {
     return processingSqlQuery($parameterList, $db);
 }
 
-// Здесь получаем список лотов
+/**
+ * Получение списка последних лотов
+ * @param int|null $limit количество получаемых лотов
+ * @param null $db подключение к ДБ
+ *
+ * @return mixed данные лота
+ */
 function getLotsList(int $limit = null, $db = null)
 {
     $sql = 'SELECT l.lot_name, l.start_price, l.img_url, l.id, MAX(b.bid_price) AS cur_price, cat.cat_name, COUNT(b.lot_id) AS bids_qty
@@ -87,7 +96,13 @@ function getLotsList(int $limit = null, $db = null)
     return processingSqlQuery($parameterList, $db);
 }
 
-// здесь получаем конкретный лот
+/**
+ * Получение данных об одном лоте по его id
+ * @param int $lot_id Цена товара точная
+ * @param null $db подключение к ДБ
+ *
+ * @return mixed данные лота
+ */
 function getLot(int $lot_id, $db = null)
 {
     $sql = 'SELECT l.lot_name, l.start_price, c.cat_name, l.id, l.img_url, l.lot_description
@@ -103,9 +118,181 @@ function getLot(int $lot_id, $db = null)
 }
 
 /**
+ * Округляет до целого цену лота
+ * @param int $price Цена товара точная
+ *
+ * @return string $price_formatted округленная цена
+ */
+function price_round($price)
+{
+    htmlspecialchars($price);
+    if ($price < 1000)
+    {
+        $price_round = $price;
+    } else
+    {
+        $price_round = number_format(ceil($price), '0', '0', '&thinsp;');
+    }
+    return $price_formatted = $price_round . ' ₽';
+};
+
+/**
+ * Сохраняет данные лота
+ * @param array $lot_data Данные лота
+ * @param array $lot_image Данные изображения
+ * @param null $db Подключение к БД
+ *
+ * @return array|int|string Id добавленного лота или массив ошибок
+ */
+function saveLot(array $lot_data, array $lot_image, $db = null)
+{
+    $errors = array_merge(checkFieldsSaveLot($lot_data), checkUplImage($lot_image, 'photo'));
+
+    if (empty($errors)) {
+        $config = getConfig();
+        if ($imageName = saveImage($lot_image, $config['imgDirUpl']))
+            $sql = 'INSERT INTO lots
+                      (lot_name, create_date, category_id, start_price, bid_step, img_url, lot_description, author_id, finish_date)
+                    VALUES 
+                      (?, NOW(), ?, ?, ?, ?, ?, 1, ?);';
+        $parametersList = [
+            'sql' => $sql,
+            'data' => [
+                $lot_data['name'],
+                $lot_data['category'],
+                $lot_data['start_price'],
+                $lot_data['step'],
+                $imageName,
+                $lot_data['description'],
+                $lot_data['finish_date']
+            ],
+            'limit' => 1
+        ];
+
+        processingSqlQuery($parametersList, $db);
+
+        return mysqli_insert_id(connectToDb());
+    } else {
+        return $errors;
+    }
+};
+
+/**
+ * Проверяет поля формы на соответствие заданному ограничению
+ * @param array $lot_data Данные лота
+ *
+ * @return array массив ошибок
+ */
+function checkFieldsSaveLot(array $lot_data) // #TODO удостовериться, что поля в форме совпадают по названию
+{
+    $errors = formRequiredFields($lot_data,
+        [
+            'name', 'category', 'description', 'start_price', 'step', 'finish_date'
+        ]); // названия полей в шаблоне
+
+    if (!getLot($lot_data['category'])) {
+        $errors['category'] = 'Выберите категорию';
+    }
+
+    if (!filter_var($lot_data['start_price'], FILTER_VALIDATE_INT) && empty($errors['start_price'])) {
+        $errors['start_price'] = 'Введите цену продажи';
+    } else {
+        if ($lot_data['start_price'] < 0) {$errors['start_price'] = 'Цена продажи должна быть больше нуля';}
+    }
+
+    if (!filter_var($lot_data['step'], FILTER_VALIDATE_INT) && empty($errors['step'])) {
+        $errors['step'] = 'Введите шаг ставки';
+    } else {
+        if ($lot_data['step'] < 1) {$errors['step'] = 'Шаг ставки должен быть больше единицы';}
+    }
+
+    if (is_numeric(strtotime($lot_data['finish_date']))) {
+        if (strtotime($lot_data['finish_date']) < time()) {
+            $errors['finish_date'] = 'Дата окончания торгов не может быть в прошлом';
+        }
+    } else {
+        $errors['finish_date'] = 'Выберите дату';
+    }
+
+    return $errors;
+};
+
+/**
+ * Формирует список проверяемых полей и проверяет их заполненность
+ * @param array $form Данные формы
+ * @param array $fields Список обязательных полей
+ *
+ * @return array массив ошибок
+ */
+function formRequiredFields(array $form, array $fields)
+{
+    $errors = [];
+
+    foreach ($fields as $field) {
+        if (empty($form[$field])) {
+            $errors[$field] = 'Поле не заполнено';
+        }
+    }
+
+    return $errors;
+}
+
+/**
+ * Проверка загружаемого изображения
+ * @param array $image Изображение
+ * @param string $key Название поля для возврата ошибки
+ *
+ * @return array $error массив ошибок
+ */
+function checkUplImage(array $image, string $key)
+{
+    $error = [];
+
+    if (empty($image['size']) or $_FILES["pictures"]["error"] != UPLOAD_ERR_OK) {  //тут потенциально может быть хрень
+        $error[$key] = 'Выберите изображение';
+    } elseif ($image['size'] > 5e+6) {
+        $error[$key] = 'Изображение не должно быть более 5Мб'; // #TODO проверить размер файла
+    } else {
+        $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+        $fileType = finfo_file($fileInfo, $image['tmp_name']); // вытаскиваем тип файла
+
+        $fileFormat = ['image/jpeg', 'image/jpg', 'image/png'];
+
+        if (!in_array($fileType, $fileFormat)) {
+            $error[$key] = 'Выберите фотографию формата JPEG, JPG или PNG';
+        }
+    }
+
+    return $error;
+};
+
+/**
+ * Сохраняет изображение на сервер
+ * @param array $image изображение для сохранения
+ * @param string $dir папка для сохранения
+ *
+ * @return bool|string Результат загрузки
+ */
+function saveImage(array $image, string $dir)
+{
+    $uploadDir = __DIR__ ; // в корне проекта
+    $name = basename($image["name"]); // здесь только имя.расширение файла
+    $uploadFile = "$uploadDir\\$dir\\$name";
+
+    if (move_uploaded_file($image['tmp_name'], "$uploadFile")) {
+        return "$dir/$name";
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Рендерит указанный шаблон
+ * @param string templ название шаблона,
+ * @return string готовый для вставки шаблон
  * @throws $e;
  */
-function renderTemplate (string $templ, $data)
+function renderTemplate(string $templ, $data)
 {
     $filePath = __DIR__ . '/templates/' . $templ . '.php';
     if (!file_exists($filePath)) {
@@ -122,16 +309,3 @@ function renderTemplate (string $templ, $data)
     }
     return ob_get_clean();
 }
-
-function price_round($price)
-{
-    htmlspecialchars($price);
-    if ($price < 1000)
-    {
-        $price_round = $price;
-    } else
-    {
-        $price_round = number_format(ceil($price), '0', '0', '&thinsp;');
-    }
-    return $price_formatted = $price_round . ' ₽';
-};
